0d2dca2ee1118c372e0a67ea65fe1c648e60176e
PragyaJ08 <pragyaj08@gmail.com>
2023-04-03 15:33:06 +0530

    Assignment3

--- Assignment3/test.py
+++ Assignment3/test.py
@@ -9,7 +9,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Defining input values to test the score function on
-message = "i like cats love them"
+msg = "i like cats love them"
 threshold = 0.7
 
 os.chdir('./Assignment3/')
@@ -19,7 +19,7 @@ test=pd.read_csv(r"./Data/Test Data.csv")
 
 # Defining input values to test the score function on
 spam = "FreeMsg Today's the day if you are ready! I'm horny & live in your town. I love sex fun & games! Netcollex Ltd 08700621170150p per msg reply Stop to end"
-nonspam = "Easy mate, * guess the quick drink was bit ambitious."
+ham = "Easy mate, * guess the quick drink was bit ambitious."
 
 # Importing Model
 filename = open("svcmodel.joblib",'rb')
@@ -31,13 +31,13 @@ os.chdir('../')
 # Defining Unit Tests
 
 # check if score function returns values properly
-def test_smoke(text=message,model=svc,threshold=threshold):
+def test_smoke(text=msg,model=svc,threshold=threshold):
     label,propensity=score.score(text,model,threshold)
     assert label!= None
     assert propensity!= None
 
 # check if the type of data meets certain requirements
-def test_format(text=message,model=svc,threshold=threshold):
+def test_format(text=msg,model=svc,threshold=threshold):
     label,propensity = score.score(text,model,threshold)
     assert type(text) == str
     assert type(threshold) == float
@@ -45,22 +45,22 @@ def test_format(text=message,model=svc,threshold=threshold):
     assert type(propensity) == float 
 
 # Checking whether propensity value between 0 or 1
-def test_pred(text=message,model=svc,threshold=threshold):
+def test_predict(text=msg,model=svc,threshold=threshold):
     label,propensity=score.score(text,model,threshold)
     assert label == False or label == True
 
 # check if the label value is in {0,1}
-def test_prop(text=message,model=svc,threshold=threshold):
+def test_propensity(text=msg,model=svc,threshold=threshold):
     label,propensity=score.score(text,model,threshold)
     assert propensity>=0 and propensity<=1
 
 # if threshold is 0, prediction becomes 1
-def test_threshold_0(text=message,model=svc,threshold=0):
+def test_0_thresh(text=msg,model=svc,threshold=0):
     label,propensity=score.score(text,model,threshold)
     assert label == 1
 
 # if threshold is 1, prediction becomes 0
-def test_threshold_1(text=message,model=svc,threshold=1):
+def test_1_thresh(text=msg,model=svc,threshold=1):
     label,propensity=score.score(text,model,threshold)
     assert label == 0
 
@@ -70,8 +70,8 @@ def test_spam(text=spam,model=svc,threshold=threshold):
     assert label == 1
 
 # testing obvious ham
-def test_nonspam(text=nonspam,model=svc,threshold=threshold):
-    label,propensity=score.score(nonspam,model,threshold)
+def test_ham(text=ham,model=svc,threshold=threshold):
+    label,propensity=score.score(ham,model,threshold)
     assert label == 0
 
 
